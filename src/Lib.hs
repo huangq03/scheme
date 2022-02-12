4186701a344cc285b449a1aa8641cf4005e00502
huangquan <huangquan@innobuddy.com>
2022-02-12 09:47:13 +0800

    Chap1: finish hello function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
     ( someFunc
     ) where
+import System.Environment
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  args <- getArgs
+  putStrLn ("Hello, " ++ args !! 0)
