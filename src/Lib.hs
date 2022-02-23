9d0abda5046ea27e3a25358a886300bbcef93934
huangquan <huangquan@innobuddy.com>
2022-02-24 07:45:10 +0800

    Finish chap 2.3: add symbol, spaces and readexpr

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,23 @@
 module Lib
     ( someFunc
+    , symbol
     ) where
+import Text.Parsec hiding (spaces)
+import Text.Parsec.String
 import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value " ++ show val
 
 someFunc :: IO ()
 someFunc = do
-  args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
