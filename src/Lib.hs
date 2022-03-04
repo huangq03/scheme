e9dd84cb020f55e5aa1d545b8c730dc5adaeda79
huangquan <huangquan@innobuddy.com>
2022-03-04 15:22:32 +0800

    Finish chap 2.3: add parseNumber, parseAtom, parseString

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 import Text.Parsec hiding (spaces)
 import Text.Parsec.String
 import System.Environment
+import Control.Monad
 
 data LispVal = Atom String
              | List [LispVal]
@@ -12,12 +13,13 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-spaces :: Parser ()
-spaces = skipMany1 space
+--spaces :: Parser ()
+--spaces = skipMany1 space
 
 parseString :: Parser LispVal
 parseString = do
@@ -26,8 +28,26 @@ parseString = do
                 _ <- char '"'
                 return $ String x
 
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value " ++ show val
 
