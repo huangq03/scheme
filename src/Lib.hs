f06af3e4d0e09cb043db18ad54689ae280a0ec4e
huangquan <huangquan@innobuddy.com>
2022-03-09 08:44:53 +0800

    Finish chap 3.1: add showVal function

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,20 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
-             deriving Show
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head' tail') = "(" ++ unwordsList head' ++ " . " ++ show tail'  ++ ")"
+
+instance Show LispVal where show = showVal
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
