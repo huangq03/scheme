11a96448e7cba69858fafb58070a9e987e11803b
huangquan <huangquan@innobuddy.com>
2022-03-12 19:45:48 +0800

    Chap4: error checking and exceptions finished

---
+++ src/Parsers/LispError.hs
@@ -0,0 +1,40 @@
+module Parsers.LispError
+( LispError (..)
+, ThrowsError
+, trapError
+, extractValue
+)
+where
+import Text.Parsec hiding (spaces)
+import Control.Monad.Except
+import Parsers.LispVal
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected 
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+showError (Default message)             = "Unknown error: " ++ message
+
+instance Show LispError where show = showError
+
+type ThrowsError = Either LispError
+
+trapError :: (MonadError a m, Show a) => m String -> m String
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right val) = val
+extractValue _ = error "extractValue exception: This shouldn't have happened. Oops."
