6bf12a1cf0253d8e70eed590851cb9cc3255c389
huangquan <huangquan@innobuddy.com>
2022-04-03 17:26:54 +0800

    Reorg code

--- src/Parsers/Helpers.hs
+++ src/Parsers/Helpers.hs
@@ -1,63 +1,19 @@
 {-# LANGUAGE ExistentialQuantification #-}
 module Parsers.Helpers
-( eval
-, isBound
+( isBound
 , getVar
 , setVar
 , defineVar
 , bindVars
 , nullEnv
 , primitives
-, apply
+, makeNormalFunc
+, makeVarArgs
 )
 where
 import Control.Monad.Except
 import Data.IORef
 import Parsers.LispVal
-
-eval :: Env -> LispVal -> IOThrowsError LispVal
-eval _ val@(String _) = return val
-eval _ val@(Number _) = return val
-eval _ val@(Bool _) = return val
-eval env (Atom id') = getVar env id'
-eval _ (List [Atom "quote", val]) = return val
-eval env (List [Atom "if", pred', conseq, alt]) =
-     do result <- eval env pred'
-        case result of
-             Bool False -> eval env alt
-             _          -> eval env conseq
-eval env (List[Atom "set!", Atom var, form]) =
-     eval env form >>= setVar env var
-eval env (List[Atom "define", Atom var, form]) =
-     eval env form >>= defineVar env var
-eval env (List (Atom "define" : List (Atom var : params') : body')) =
-     makeNormalFunc env params' body' >>= defineVar env var
-eval env (List (Atom "define" : DottedList (Atom var : params') varargs : body')) =
-     makeVarArgs varargs env params' body' >>= defineVar env var
-eval env (List (Atom "lambda" : List params' : body')) =
-     makeNormalFunc env params' body'
-eval env (List (Atom "lambda" : DottedList params' varargs : body')) =
-     makeVarArgs varargs env params' body'
-eval env (List (function : args)) = do
-     func <-  eval env function
-     argVars <- mapM (eval env) args
-     apply func argVars
-eval _ badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
-
-apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
-apply (PrimitiveFunc func) args = liftThrows $ func args
-apply (Func params' varargs body' closure') args =
-      if num params' /= num args && varargs == Nothing
-         then throwError $ NumArgs (num params') args
-         else (liftIO $ bindVars closure' $ zip params' args) >>= bindVarArgs varargs >>= evalBody
-      where remainingArgs = drop (length params') args
-            num = toInteger . length
-            evalBody env = liftM last $ mapM (eval env) body'
-            bindVarArgs arg env = case arg of
-                Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
-                Nothing -> return env
-apply (IOFunc func) args = func args
-apply nonFunc _ = throwError $ NotFunction "Unsupported function type" (show nonFunc)
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [("+", numericBinop (+)),
